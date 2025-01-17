e61ebfab0c99dfdd8501d5261f3956ca969b7c7b
RJ Pearsall <rjpearsall@RJs-MacBook-Pro.local>
2025-01-16 23:06:34 -0500

    added arrays to test sciview

--- array.py
+++ array.py
@@ -1,3 +1,14 @@
-print("this is array.py")
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
 
-print("this is another test")
+# Create a NumPy array
+array = np.random.rand(20, 3)  # 10 rows, 3 columns of random numbers
+print("NumPy Array:\n", array)
+
+# View the array in SciView
+# Right-click on 'array' in the console and select "View as Array" in PyCharm.
+
+# Convert the array to a Pandas DataFrame
+df = pd.DataFrame(array, columns=['A', 'B', 'C'])
+print("\nPandas DataFrame:\n", df)
